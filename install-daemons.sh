ee30060531eb3cda4b4244f31d8b2c36e9a98c78
Sean Pargeter <spargete@uoregon.edu>
2017-01-13 15:30:16 -0800

    Curl needs to work right if you'd believe it

--- install-daemons.sh
+++ install-daemons.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/postgres/postgres -b REL9_5_STABLE --single-branch $HOME/postgres
 
-curl http://www.gtlib.gatech.edu/pub/apache//httpd/httpd-2.4.25.tar.bz2
+curl http://www.gtlib.gatech.edu/pub/apache//httpd/httpd-2.4.25.tar.bz2 > httpd-2.4.25.tar.bz2
 tar -xjf httpd-2.4.25
 
 cd postgres
