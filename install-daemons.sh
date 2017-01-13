96ac6465b183c9483f2a2a03384d021286fb0802
Sean Pargeter <spargete@uoregon.edu>
2017-01-13 15:16:07 -0800

    adding install-daemons.sh to clone to osnap

---
+++ install-daemons.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/postgres/postgres
+
+curl http://www.gtlib.gatech.edu/pub/apache//httpd/httpd-2.4.25.tar.bz2
+tar -xjf httpd-2.4.25
+
+cd postgres
+./configure --prefix=$1
+make
+make install
+
+cd ../httpd-2.4.25
+./configure --prefix=$1
+make
+make install
