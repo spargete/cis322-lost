067ac7f5dff756fcbd31ace10742d57ee26ef8e6
Sean Pargeter <spargete@uoregon.edu>
2017-01-13 15:37:34 -0800

    make tar work, make the directory changing work

--- install-daemons.sh
+++ install-daemons.sh
@@ -1,7 +1,9 @@
 git clone https://github.com/postgres/postgres -b REL9_5_STABLE --single-branch $HOME/postgres
 
 curl http://www.gtlib.gatech.edu/pub/apache//httpd/httpd-2.4.25.tar.bz2 > httpd-2.4.25.tar.bz2
-tar -xjf httpd-2.4.25
+tar -xjf httpd-2.4.25.tar.bz2
+
+cd $HOME
 
 cd postgres
 ./configure --prefix=$1
