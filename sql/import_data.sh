e0d11b4ce370f21ada53728cf6bf8b020a68c2ca
Sean Pargeter <spargete@uoregon.edu>
2017-01-26 14:09:26 -0800

    changed to relative paths--I don't like it very much because no guarantees that there are write permissions everywhere but that's what I was told to do

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -1,15 +1,15 @@
 #Curl the legacy data
-curl https://classes.cs.uoregon.edu/17W/cis322/files/osnap_legacy.tar.gz > $HOME/osnap_legacy.tar.gz
+curl https://classes.cs.uoregon.edu/17W/cis322/files/osnap_legacy.tar.gz > ../../osnap_legacy.tar.gz
 #extract it
-cd $HOME
+cd ../..
 tar -xzf osnap_legacy.tar.gz
 
 #to run a command without jumping into the thing do psql -c "command"
 #to use a file as a command use -f filename
 cd osnap_legacy
 
-psql -d $1 -p $2 -f $HOME/cis322-lost/sql/create_tables.sql
-python3 $HOME/cis322-lost/sql/import_data.py > import_data.sql
+psql -d $1 -p $2 -f ../cis322-lost/sql/create_tables.sql
+python3 ../../cis322-lost/sql/import_data.py > import_data.sql
 #Here the python script just has to print the import statements
 psql -d $1 -p $2 -f import_data.sql
 rm import_data.sql
