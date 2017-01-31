82a9b9d620db775c3f19adb3b72f29f12b131e2e
Sean Pargeter <spargete@uoregon.edu>
2017-01-31 09:01:48 -0800

    confirming form templating works

--- src/wsgi/app.py
+++ src/wsgi/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/report_filter')
+def report_filter():
+	return render_template('report_filter.html')
