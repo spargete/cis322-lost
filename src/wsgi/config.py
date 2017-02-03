b987555ca2ad26d3d6f93bce7ad19293a1ee0b62
Sean Pargeter <spargete@uoregon.edu>
2017-02-03 09:44:44 -0800

    config indentation fixing

--- src/wsgi/config.py
+++ src/wsgi/config.py
@@ -6,5 +6,5 @@ with open(lost_config.json) as conf:
 	c = json.load(conf)
 	dbname = c['database']['dbname']
 	dbhost = c['database']['dbhost']
-    dbport = c['database']['dbport']
-    secret_key = c['session']['secret_key']
+	dbport = c['database']['dbport']
+	secret_key = c['session']['secret_key']
