0677aa11cd355f31097af560efeeb67dcc042575
Sean Pargeter <spargete@uoregon.edu>
2017-02-03 09:54:30 -0800

    adding the file path argument from the given example, but I don't know why it works

--- src/wsgi/config.py
+++ src/wsgi/config.py
@@ -2,7 +2,9 @@ import json
 import os
 import pathlib
 
-with open(lost_config.json) as conf:
+cpath = pathlib.Path(os.path.realpath(__file__)).parent.joinpath('lost_config.json')
+
+with cpath.open() as conf:
 	c = json.load(conf)
 	dbname = c['database']['dbname']
 	dbhost = c['database']['dbhost']
