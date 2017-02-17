f4f043eef8848b057cfe94817c77c9b86f849ae4
Sean Pargeter <spargete@uoregon.edu>
2017-02-17 10:00:31 -0800

    Step 5 ready for testing

--- src/wsgi/app.py
+++ src/wsgi/app.py
@@ -22,10 +22,33 @@ def create_user():
 		if result == None:
 			cur.execute('INSERT INTO users (username, password) VALUES (%s, %s)', (username, password))
 			conn.commit()
+			cur.close()
 			conn.close()
 			return render_template('user_added.html')
 		else:
+			cur.close()
 			conn.close()
 			return render_template('user_exists.html')
 
 	return render_template('create_user.html')
+
+@app.route('/')
+@app.route('/login', methods ['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		conn.psycopg2.connect(dbname=dbname, host=dbhost, port=dbport)
+		cur = conn.cursor()
+		username = request.form['username']
+		password = request.form['password']
+		cur.execute('SELECT username, password FROM users WHERE username=%s AND password=%s', (username, password))
+		try:
+			result = cur.fetchone()
+		except ProgrammingError:
+			result = None
+
+		if result == None:
+			return render_template('incorrect_credentials.html')
+		else:
+			session['username'] = username
+			session['logged_in'] = True
+			return redirect(url_for('dashboard'))
